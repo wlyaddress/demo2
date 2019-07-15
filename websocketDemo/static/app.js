01ecdbee6ae4c31316b4ba484f95a570369e8e93
wlyaddress <13380314014@163.com>
2019-07-15 08:58:45 +0800

    websocket例子

---
+++ websocketDemo/static/app.js
@@ -0,0 +1,54 @@
+var stompClient = null;
+var haha="1";
+var stompUrl='/user/'+haha+'/one';
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+	var userId = 1;
+    var socket = new SockJS('http://192.168.16.71:801/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+		stompClient.subscribe('/topic/getResponse' , function (greeting) {
+			console.log("********"+greeting);
+            showGreeting(greeting);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/welcome", {}, $("#name").val());
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
